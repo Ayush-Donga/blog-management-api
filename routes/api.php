ec27146e6759c83291e8405467b29d440af21511
Ayush Donga <ayush.donga@gmail.com>
2025-11-03 18:10:49 +0530

    Set up API routes

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BlogController;
 
-// Public route
+// LOGIN-API (public)
 Route::post('/login', [AuthController::class, 'login']);
 
-// Protected routes
+// Protected blog APIs (require login)
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/blogs', [BlogController::class, 'store']);
-    Route::get('/blogs', [BlogController::class, 'index']);
-    Route::put('/blogs/{id}', [BlogController::class, 'update']);
-    Route::delete('/blogs/{id}', [BlogController::class, 'destroy']);
-    Route::post('/blogs/{id}/like-toggle', [BlogController::class, 'toggleLike']);
+    Route::post('/blogs', [BlogController::class, 'store']); // CREATE
+    Route::get('/blogs', [BlogController::class, 'index']);  // LIST
+    Route::put('/blogs/{id}', [BlogController::class, 'update']); // EDIT
+    Route::delete('/blogs/{id}', [BlogController::class, 'destroy']); // DELETE
+    Route::post('/blogs/{id}/like-toggle', [BlogController::class, 'toggleLike']); // LIKE TOGGLE
 });
