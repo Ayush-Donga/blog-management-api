bab473ced4d9113f7acdb78641dcd76965f3344e
Ayush Donga <ayush.donga@gmail.com>
2025-11-03 18:02:51 +0530

    Created models with polymorphic relationships

---
+++ app/Models/Blog.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Support\Facades\Storage;
+
+class Blog extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'description', 'image_path', 'user_id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function likes(): MorphMany
+    {
+        return $this->morphMany(Like::class, 'likeable');
+    }
+
+    // Accessor for full image URL
+    public function getImagePathAttribute($value)
+    {
+        return $value ? Storage::url($value) : null;
+    }
+}
